72342c3cd22a2feb14e007299d72b4399c5ec965
renjithsamuel <ranjithsamuelking@gmail.com>
2023-04-19 19:19:59 +0530

    Fixing few bugs

--- Server/controller/Bookscontroller.js
+++ Server/controller/Bookscontroller.js
@@ -22,7 +22,7 @@ exports.getBooks = async (req,res,next) => {
     exports.postBooks = async (req,res,next) =>{
         // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         console.log(req.body);
-        if(req.body.title == null || req.body.author == null || req.body.subject == null || req.body.publishDate == null || req.book.desc == null){
+        if(req.body.title == null || req.body.author == null || req.body.subject == null || req.body.publishDate == null || req.body.desc == null){
             return res.status(400).send("Send correct values");
         }
         try{
